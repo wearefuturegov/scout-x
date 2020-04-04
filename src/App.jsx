6b587b181d809b7726e8e7681179e4c0a20a8994
Jaye Hackett <jayehackett@FutureGovs-MacBook-Pro.local>
2020-04-04 21:29:15 +0100

    header and footer styled

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,23 @@
 import React from "react"
+import theme from "./components/_theme"
+import { createGlobalStyle } from "styled-components"
+import Layout from "./components/Layout"
+
+const GlobalStyle = createGlobalStyle`
+  *{
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    font-family: Helvetica Neue, Arial, Helvetica, sans-serif;
+  }
+`
 
 const App = () =>
-  <div>
-    Test
-  </div>
+  <>
+    <GlobalStyle/>
+    <Layout>
+      Test
+    </Layout>
+  </>
 
 export default App
---
+++ src/components/_theme.js
@@ -0,0 +1,17 @@
+export default {
+    link: "#2C2D84",
+    text: "#212121",
+    grey: "#343434",
+    pale: "#f7f6f2",
+    white: "#ffffff",
+
+    focus: "#FFB81C",
+    error: "#d51010",
+
+    
+    maxWidth: "1200px",
+    outerSpacing: "20px",
+
+    breakpointM: "750px",
+    breakpointL: "1000px"
+}
