7b33851e095cad5f9deb53b63dbdf69580f85563
Jaye <joshua.hackett93@gmail.com>
2020-05-05 17:13:11 +0100

    react-router -> reach-router

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from "react"
-import { useLocation, useNavigate } from "@reach/router"
 import fetch from "isomorphic-unfetch"
 import queryString from "query-string"
 import Layout, { ResultsList, Count } from "./components/Layout"
@@ -10,84 +9,81 @@ import Filters from "./components/Filters"
 import Filter from "./components/Filter"
 import config from "./_config"
 
-const App = ( props) => {
+const App = ({
+  children,
+  location,
+  navigate
+}) => {
 
-  console.log("PROPS: ", props)
+  const originalQuery = queryString.parse(location.search)
 
-  // let navigation = useNavigate()
-  // let location = useLocation()
+  const [collection, setCollection] = useState(originalQuery.collection || "services")
+  const [coverage, setCoverage] = useState(originalQuery.coverage || "")
+  const [categories, setCategories] = useState([].concat(originalQuery.categories))
+  const [only, setOnly] = useState([].concat(originalQuery.only))
 
-  // // console.log("NAVIGATION:", navigation, "LOCATION:", location)
+  const [results, setResults] = useState(false)
 
-  // const originalQuery = queryString.parse(location.search)
+  useEffect(() => {
+    setResults(false)
+    // 1. Form new query
+    let newQuery = {
+      categories,
+      only,
+      collection,
+      coverage
+    }
+    // 2. Update URL
+    navigate(`/?${queryString.stringify(newQuery)}`)
+    // 3. Retrieve new results
+    fetch(`${process.env.REACT_APP_API_HOST}/services?${queryString.stringify(newQuery)}`)
+      .then(res => res.json())
+      .then(data => setResults(data.content))
+  }, [categories, only, coverage, collection])
 
-  // const [collection, setCollection] = useState(originalQuery.collection || "services")
-  // const [coverage, setCoverage] = useState(originalQuery.coverage || "")
-  // const [categories, setCategories] = useState([].concat(originalQuery.categories))
-  // const [only, setOnly] = useState([].concat(originalQuery.only))
-
-  // const [results, setResults] = useState(false)
-
-  // useEffect(() => {
-  //   setResults(false)
-  //   // 1. Form new query
-  //   let newQuery = {
-  //     categories,
-  //     only,
-  //     collection,
-  //     coverage
-  //   }
-  //   // 2. Update URL
-  //   // location.replace(`/?${queryString.stringify(newQuery)}`)
-  //   // 3. Retrieve new results
-  //   fetch(`${process.env.REACT_APP_API_HOST}/services?${queryString.stringify(newQuery)}`)
-  //     .then(res => res.json())
-  //     .then(data => setResults(data.content))
-  // }, [categories, only, coverage, collection])
-
-  // return(
-  //   <>
-  //     <Layout
-  //       headerComponents={
-  //         <SearchBar
-  //           type={collection}
-  //           setType={setCollection}
-  //           coverage={coverage}
-  //           setCoverage={setCoverage}
-  //         />
-  //       }
-  //       sidebarComponents={<>
-  //         <Filters>
-  //           <Filter
-  //             legend="Categories"
-  //             options={config.categories}
-  //             selection={categories}
-  //             setSelection={setCategories}
-  //           />
-  //           <Filter
-  //             legend="Only show"
-  //             options={config.only}
-  //             selection={only}
-  //             setSelection={setOnly}
-  //           />
-  //         </Filters>
-  //       </>}
-  //       mainContentComponents={<>
-  //         <Count>Showing {results.length} results near <strong>XXX</strong></Count>
-  //         <ResultsList aria-live="polite">
-  //           {results ?
-  //             results.map(s =>
-  //               <ServiceCard key={s.id} {...s}/>  
-  //             )
-  //           : 
-  //           <Skeleton/>
-  //           }
-  //         </ResultsList>
-  //       </>}
-  //     />
-  //     {children}
-  //   </>
-  // )
+  return(
+    <>
+      <Layout
+        headerComponents={
+          <SearchBar
+            type={collection}
+            setType={setCollection}
+            coverage={coverage}
+            setCoverage={setCoverage}
+          />
+        }
+        sidebarComponents={<>
+          <Filters>
+            <Filter
+              legend="Categories"
+              options={config.categories}
+              selection={categories}
+              setSelection={setCategories}
+            />
+            <Filter
+              legend="Only show"
+              options={config.only}
+              selection={only}
+              setSelection={setOnly}
+            />
+          </Filters>
+        </>}
+        mainContentComponents={<>
+          <Count>Showing {results.length} results near <strong>XXX</strong></Count>
+          <ResultsList aria-live="polite">
+            {results ?
+              results.map(s =>
+                <ServiceCard key={s.id} {...s}/>  
+              )
+            : 
+            <Skeleton/>
+            }
+          </ResultsList>
+        </>}
+      />
+      {children}
+    </>
+  )
 }
 
 export default App
