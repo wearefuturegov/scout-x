cd96acc248cd021757a3106cfc4cd910f156dd2d
Jaye Hackett <jayehackett@FutureGovs-MacBook-Pro.local>
2020-05-06 20:37:32 +0100

    break map into object

--- src/components/DetailDialog/Map.jsx
+++ src/components/DetailDialog/Map.jsx
@@ -1,6 +1,7 @@
 import React from "react"
+import queryString from "query-string"
 import styled from "styled-components"
-import marker from "./marker.png"
+// import marker from "./marker.png"
 
 const Img = styled.img`
     width: 100%;
@@ -9,10 +10,18 @@ const Img = styled.img`
 const Map = ({
     latitude,
     longitude
-}) =>
-    <Img 
-        src={`https://maps.googleapis.com/maps/api/staticmap?scale=2&key=${process.env.REACT_APP_GOOGLE_CLIENT_KEY}&size=1200x270&markers=${latitude},${longitude}`} 
+}) => {
+    const query = queryString.stringify({
+        scale: 2,
+        key: process.env.REACT_APP_GOOGLE_CLIENT_KEY,
+        size: "1200x270",
+        markers: `${latitude},${longitude}`
+    })
+
+    return <Img 
+        src={`https://maps.googleapis.com/maps/api/staticmap?${query}`} 
         alt=""
         />
+}
 
 export default Map
