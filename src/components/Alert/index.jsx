701b27bbbe4b53d500ce82818219590c7d6050ba
Jaye Hackett <jayehackett@FutureGovs-MacBook-Pro.local>
2020-05-10 17:42:45 +0100

    style alerts a 'lil

--- src/components/Alert/index.jsx
+++ src/components/Alert/index.jsx
@@ -13,22 +13,25 @@ const Outer = styled.div`
 
 const StyledAlert = styled(Alert)`
     background: ${theme.text};
-    padding: 15px;
+    padding: 20px;
     color: ${theme.white};
     font-weight: bold;
     pointer-events: none;
-    box-shadow: 0px 2px 12px ${theme.textBackground};
+    font-size: 0.9rem;
+    box-shadow: 0px 2px 12px rgba(0,0,0,0.1);
     animation: popIn 5s ease-out;
+    text-align: center;
     @media screen and (min-width: ${theme.breakpointM}){
         max-width: 400px;
         margin: 0 auto 20px auto;
+        text-align: left;
     }
     @keyframes popIn{
         0%{
             opacity: 0;
             transform: translateY(10px);
         }
-        3%{
+        2%{
             opacity: 1;
             transform: translateY(0px);
         }
