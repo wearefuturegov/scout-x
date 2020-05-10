fb75c4699c8415225564141761690d88356be126
Jaye Hackett <jayehackett@FutureGovs-MacBook-Pro.local>
2020-05-10 21:32:12 +0100

    the suspense is killing me

--- src/index.js
+++ src/index.js
@@ -1,32 +1,27 @@
 import "react-app-polyfill/ie11"
 import "react-app-polyfill/stable"
 
-import React, { lazy, Suspense } from "react"
+import React from "react"
 import ReactDOM from "react-dom"
 import App from "./App"
 import { Router } from "@reach/router"
+import DetailDialog from "./components/DetailDialog"
+import PinboardDialog from "./components/PinboardDialog"
 import { GoogleContextProvider } from "./contexts/googleContext"
 import { AlertContextProvider } from "./contexts/alertContext"
 import { PinboardContextProvider } from "./contexts/pinboardContext"
-
-const DetailDialog = lazy(() => import("./components/DetailDialog"))
-const PinboardDialog = lazy(() => import("./components/PinboardDialog"))
-
-const loader = () => <p>Loading...</p>
 
 ReactDOM.render(
   <React.StrictMode>
     <PinboardContextProvider>
       <AlertContextProvider>
         <GoogleContextProvider>
-        <Suspense fallback={loader}>
           <Router>
             <App path="/" default >
-                <DetailDialog path="service/:serviceId"/>
-                <PinboardDialog path="pinboard"/>
+              <DetailDialog path="service/:serviceId"/>
+              <PinboardDialog path="pinboard"/>
             </App>
           </Router>
-          </Suspense>
         </GoogleContextProvider>
       </AlertContextProvider>
     </PinboardContextProvider>
