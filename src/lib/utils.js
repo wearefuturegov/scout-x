473182f79d97d3c30277d918f353daec44ae1fd8
Jaye Hackett <jayehackett@FutureGovs-MacBook-Pro.local>
2020-05-13 21:41:41 +0100

    show real taxa and distance on service cards

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -7,3 +7,12 @@ export const truncate = (str, noWords) => {
         }
     }
 }
+
+export const prettyDistance = miles => {
+    if(miles !== undefined){
+        let roundMiles = Math.round(miles)
+        if(roundMiles < 1 || roundMiles === 0) return "Less than a mile away"
+        if(roundMiles === 1) return "About a mile away"
+        return `About ${roundMiles} miles away`
+    }
+}
