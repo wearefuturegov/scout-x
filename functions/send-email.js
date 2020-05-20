4eeac4233187b969eec3cbc985e4b9fdac251a8f
Jaye Hackett <jayehackett@FutureGovs-MacBook-Pro.local>
2020-05-21 00:38:05 +0100

    add email sending

--- functions/send-email.js
+++ functions/send-email.js
@@ -1,26 +1,53 @@
-const fetch = require("isomorphic-unfetch")
 const mail = require("@sendgrid/mail")
 
 exports.handler = async (event, context, callback) => {
-    let { email, pins } = JSON.parse(event.body)
+    try{
+        let { email, pins } = JSON.parse(event.body)
 
-    const msg = {
-        to: email,
-        from: 'test@example.com',
-        subject: 'Your pinned services',
-        html: '<strong>and easy to do anywhere, even with Node.js</strong>',
+        let host = process.env.EMAIL_HOST || "https://scout-x.netlify.app/service/"
+
+        mail.setApiKey(process.env.SENDGRID_API_KEY)
+        const msg = {
+            to: email,
+            from: process.env.EMAIL_FROM || "CHANGEME@CHANGEME.com",
+            subject: "Your pinned services",
+            template_id: process.env.SENDGRID_TEMPLATE_ID,
+            dynamic_template_data: { 
+                "pins": pins.map(pin =>
+                    `<div style="margin-bottom: 25px;">
+                        <h2>
+                            <a href="${host}${pin.id}">${pin.name}</a>
+                        </h2>
+                        <p>${truncate(pin.description, 30)}</p>
+                        ${pin.website ? `<p><a href="${pin.website}">Visit website</a></p>` : ""}
+                        ${pin.email ? `<p><a href="mailto:${pin.email}">Send email</a></p>` : ""}
+                    </div>`    
+                )
+            }
+        }
+        mail.send(msg)
+        return {
+            statusCode: 200,
+            body: JSON.stringify({
+                status: "OK"
+            })
+        }
+    } catch(e){
+        return {
+            statusCode: 500,
+            body: JSON.stringify({
+                error: e
+            })
+        }
     }
-    mail.send(msg)
+}
 
-
-    return {
-        statusCode: 200,
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({
-            query: event.queryStringParameters,
-            data: event.body
-        })
+const truncate = (str, noWords) => {
+    if(str && (noWords > 1)){
+        if(str.split(" ").length > noWords){
+            return str.split(" ").splice(0,noWords).join(" ") + "..."
+        } else {
+            return str
+        }
     }
 }
