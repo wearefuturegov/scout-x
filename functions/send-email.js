3f11810aa6189615faa5bee3013c6c4d30f69c51
Jaye Hackett <jayehackett@FutureGovs-MacBook-Pro.local>
2020-05-21 10:20:52 +0100

    refactor email send job

--- functions/send-email.js
+++ functions/send-email.js
@@ -1,4 +1,5 @@
-const mail = require("@sendgrid/mail")
+const fetch = require("isomorphic-unfetch")
+const template = require("./template")
 
 exports.handler = async (event, context, callback) => {
     try{
@@ -6,39 +7,40 @@ exports.handler = async (event, context, callback) => {
 
         let host = process.env.EMAIL_HOST || "https://scout-x.netlify.app/service/"
 
-        console.log("API KEY: ", process.env.SENDGRID_API_KEY)
-        mail.setApiKey(process.env.SENDGRID_API_KEY)
-        const msg = {
-            to: email,
-            from: process.env.EMAIL_FROM || "CHANGEME@CHANGEME.com",
-            subject: "Your pinned services",
-            template_id: process.env.SENDGRID_TEMPLATE_ID,
-            dynamic_template_data: { 
-                "pins": pins.map(pin =>
-                    `<div style="margin-bottom: 25px;">
-                        <h2>
-                            <a href="${host}${pin.id}">${pin.name}</a>
-                        </h2>
-                        <p>${truncate(pin.description, 30)}</p>
-                        ${pin.website ? `<p><a href="${pin.website}">Visit website</a></p>` : ""}
-                        ${pin.email ? `<p><a href="mailto:${pin.email}">Send email</a></p>` : ""}
-                    </div>`    
-                )
-            }
-        }
-        console.log(msg)
-        mail.send(msg)
-            .then(() => {
-            // Celebrate
-            })
-            .catch(error => {
-            // Log friendly error
-                console.error(error);
-            });
-        return {
-            statusCode: 200,
+        let res = await fetch("https://api.sendgrid.com/v3/mail/send", {
+            method: "POST",
+            headers: {
+                'Authorization': 'Bearer ' + [process.env.SENDGRID_API_KEY],
+                'content-type': 'application/json'
+            },
             body: JSON.stringify({
-                status: "OK"
+                "from": {
+                    "email": process.env.EMAIL_FROM || "CHANGEME@CHANGEME.com"
+                },
+                "subject": "Your pinned services",
+                "personalizations": [
+                    {
+                        "to": [
+                            {
+                                "email": email
+                            }
+                        ]
+                    }
+                ],
+                "content": [
+                    {
+                      "type": "text/html",
+                      "value": template(host, pins)
+                    }
+                ]
+            })
+        })
+
+
+        return {
+            statusCode: res.status,
+            body: JSON.stringify({
+                status: res.body
             })
         }
     } catch(e){
@@ -52,12 +54,3 @@ exports.handler = async (event, context, callback) => {
     }
 }
 
-const truncate = (str, noWords) => {
-    if(str && (noWords > 1)){
-        if(str.split(" ").length > noWords){
-            return str.split(" ").splice(0,noWords).join(" ") + "..."
-        } else {
-            return str
-        }
-    }
-}
