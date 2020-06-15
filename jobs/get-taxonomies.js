bf3e774d47f15bb4ca9aeac642310d91c541c97d
Jaye Hackett <jayehackett@FutureGovs-MacBook-Pro.local>
2020-06-15 21:16:43 +0100

    fix taxonomies job

--- jobs/get-taxonomies.js
+++ jobs/get-taxonomies.js
@@ -4,7 +4,7 @@ const fs = require("fs")
 
 const fetchTaxonomies = async () => {
     console.log("Fetching taxonomy data...")
-    const res = await fetch(`${process.env.REACT_APP_API_HOST}/taxonomies`)
+    const res = await fetch(process.env.TAXONOMIES_DATASOURCE)
     const data = await res.json()
     fs.writeFile("src/data/_taxonomies.json", JSON.stringify(data), err => {
         if (err) return console.log(err)
