93d9227465219b519fb54db09a7cb7f698983741
Jaye <joshua.hackett93@gmail.com>
2020-07-02 13:44:43 +0100

    clear out old service dialog code

--- src/components/DetailDialog/index.jsx
+++ src/components/DetailDialog/index.jsx
@@ -49,14 +49,8 @@ const DetailDialog = ({
             <Header>
                 {service.organisation.name && <Caption>{service.organisation.name}</Caption>}
                 <Title>{service.name}</Title>
-                <Tags>
-                    {service.taxonomies.map(taxonomy =>
-                        <Tag key={taxonomy.id}>{taxonomy.name}</Tag>
-                    )}
-                </Tags>
             </Header>
-
-            <Location>
+            {/* <Location>
                 <Map
                     latitude={parseFloat(service.locations[0].latitude)}
                     longitude={parseFloat(service.locations[0].longitude)}
@@ -68,50 +62,14 @@ const DetailDialog = ({
                     <p>{service.locations[0].postal_code}</p>
                     <p><A href={`https://maps.google.com/maps/search/${service.locations[0].postal_code}`}>Get directions</A></p>
                 </LocationInner>
-            </Location>
-
-            
+            </Location> */}
             <Body>
                 <Actions>
                     {service.url && <ButtonLink href={service.url}>Visit website</ButtonLink>}
                     <PinboardButton service={service}/>
                 </Actions>
                 {service.description && <Description description={service.description}/>}
-
-                <SplitContent>
-                    <SplitContentSection>
-                        <Crosshead>When</Crosshead>
-                        <p>Content goes here</p>
-                    </SplitContentSection>
-                    <SplitContentSection>
-                        <Crosshead>Contact</Crosshead>
-                        {/* {service.contacts.length > 0 &&
-                            <>
-                                <p><A href="mailto:{service.email}">{service.email}</A></p>
-                                <p><em>{service.contacts[0].name}, {service.contacts[0].title}</em></p>
-                                <p>{service.contacts[0].phones[0].number}</p>
-                            </>
-                        } */}
-                    </SplitContentSection>
-                </SplitContent>
             </Body>
-            <DarkBody>
-            <SplitContent>
-                    <SplitContentSection>
-                        <Crosshead>Accessibility</Crosshead>
-                        <p>Content here</p>
-                    </SplitContentSection>
-                    <SplitContentSection>
-                        <Crosshead>SEND Needs</Crosshead>
-                        <p>Content here</p>
-                    </SplitContentSection>
-                </SplitContent>
-            </DarkBody>
-            <Disclaimer>
-                <p>We regularly check and update these community services, but can’t guarantee that they will always be accurate.</p>
-                <p>If anything here is out of date or missing, please <A target="_blank" href={`https://outpost-staging.herokuapp.com/services/${service.id}/feedbacks`}>suggest an edit</A>.</p>
-                <p>You may need a referral for some activities and groups. Contact the organiser if unsure.</p>
-            </Disclaimer>
         </Dialog>
         :
         <Loader/>
