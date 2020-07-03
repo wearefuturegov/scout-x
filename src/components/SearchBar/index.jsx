0869894b2bff22394206309be62c096754818abb
Jaye <joshua.hackett93@gmail.com>
2020-07-03 13:31:46 +0100

    refactor query hook to clean up falsy parameters

--- src/components/SearchBar/index.jsx
+++ src/components/SearchBar/index.jsx
@@ -33,31 +33,6 @@ const Label = styled.label`
     display: inline-block;
 `
 
-// const Select = styled.select`
-//     font-size: 1rem;
-//     padding: 10px;
-//     border: 2px solid ${theme.text};
-//     display: block;
-//     width: 100%;
-//     border-radius: 0px;
-//     appearance: none;
-//     -moz-appearance: none;
-//     -ms-appearance: none;
-//     -webkit-appearance: none;
-//     background: ${theme.white};
-//     background-image: url("data:image/svg+xml,%3Csvg fill='none' height='31' viewBox='0 0 42 31' width='42' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='m0 0h42v31h-42z' fill='%23fff'/%3E%3Cpath clip-rule='evenodd' d='m20.9393 21-9.0606-9.0607 2.1213-2.12128 6.9393 6.93938 6.9394-6.93938 2.1213 2.12128z' fill='%23212121' fill-rule='evenodd'/%3E%3C/svg%3E");
-//     background-position: center right;
-//     background-repeat: no-repeat;
-//     padding-right: 45px;
-//     height: 45px;
-//     &:focus{
-//         outline: 3px solid ${theme.focus};
-//     }
-//     &::-ms-expand {
-//         display: none;
-//     }
-// `
-
 const GeolocateButton = styled.button`
     position: absolute;
     right: 2px;
