4b078b72700cd9d032c2e091ab533e5a583368ac
Jaye Hackett <jayehackett@FutureGovs-MacBook-Pro.local>
2020-11-27 17:05:44 +0000

    bring back "only" free filter

--- src/data/_config.js
+++ src/data/_config.js
@@ -1,18 +1,14 @@
 export const onlyOptions = [
     {
-        label: "Open after 6pm",
-        slug: "open-after-6pm"
-    },
-    {
-        label: "Open weekends",
-        slug: "open-weekends"
-    },
-    {
         label: "Free",
         slug: "free"
     },
-    {
-        label: "Spaces for new children",
-        slug: "vacancies"
-    }
+    // {
+    //     label: "Open after 6pm",
+    //     slug: "open-after-6pm"
+    // },
+    // {
+    //     label: "Open weekends",
+    //     slug: "open-weekends"
+    // },
 ]
