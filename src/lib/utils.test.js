e5d8efcf38521d3caacc9106d22a10f8294ce3f7
Jaye <joshua.hackett93@gmail.com>
2021-01-13 13:49:34 +0000

    better test format

--- src/lib/utils.test.js
+++ src/lib/utils.test.js
@@ -1,34 +1,37 @@
 import { truncate, 
-    prettyDistance, 
-    // daysSince, 
-    // wheelchairAccessible, 
-    // openWeekends, 
-    // openAfterSix, 
-    // twelveHourTime
+    prettyDistance,
+    twelveHourTime
 } from "./utils"
 
 describe("truncate", () => {
     it("leaves short text unaltered", () => {
-        const output = truncate("Example input", 2)
-        expect(output).toBe("Example input")
+        expect(truncate("Example input", 2)).toBe("Example input")
     })
     it("truncates longer text", () => {
-        const output = truncate("Example input example input", 2)
-        expect(output).toBe("Example input...")
+        expect(truncate("Example input example input", 2)).toBe("Example input...")
     })
 })
 
 describe("prettyDistance", () => {
     it("displays short distances properly", () => {
-        const output = prettyDistance(0.4)
-        expect(output).toBe("Less than a mile away")
+        expect(prettyDistance(0.4)).toBe("Less than a mile away")
     })
     it("displays medium distances properly", () => {
-        const output = prettyDistance(1)
-        expect(output).toBe("About a mile away")
+        expect(prettyDistance(1)).toBe("About a mile away")
     })
     it("displays longer distances properly", () => {
-        const output = prettyDistance(100)
-        expect(output).toBe("About 100 miles away")
+        expect(prettyDistance(100)).toBe("About 100 miles away")
+    })
+})
+
+describe("twelveHourTime", () => {
+    it("displays mornings properly", () => {
+        expect(twelveHourTime("10:00")).toBe("10.00 am")
+    })
+    it("displays afternoons properly", () => {
+        expect(twelveHourTime("17:00")).toBe("5.00 pm")
+    })
+    it("displays midnight properly", () => {
+        expect(twelveHourTime("00:00")).toBe("12.00 am")
     })
 })
