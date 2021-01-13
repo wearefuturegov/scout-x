6a137b561cb0d2133e5791ffa5b63c0113740acd
Jaye Hackett <jayehackett@FutureGovs-MacBook-Pro.local>
2021-01-13 16:39:28 +0000

    add first spec and ci

--- cypress/integration/results_list_spec.js
+++ cypress/integration/results_list_spec.js
@@ -1,3 +1,20 @@
-describe("List page", () => {
-  it("Renders a list of results", () => {})
+describe("Results list page", () => {
+  beforeEach(() => {
+    cy.visit("/")
+
+    cy.intercept("/api/v1/services", {
+      fixture: "services",
+    })
+  })
+
+  it("renders a list of results", () => {
+    cy.visit("/")
+    cy.get("h1").should("contain", "Find activities and organisations near you")
+
+    cy.get("ul li").should("have.length", 20)
+
+    // cy.get("ul")
+    //   .should("contain", "12th Aylesbury Brownies")
+    //   .should("contain", "Chepping View Primary Academy")
+  })
 })
