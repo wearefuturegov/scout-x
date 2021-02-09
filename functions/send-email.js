22036fe17106a4b31159a8010daa1c5ccce8df15
Jaye <joshua.hackett93@gmail.com>
2021-02-09 13:37:11 +0000

    emails wip

--- functions/send-email.js
+++ functions/send-email.js
@@ -1,7 +1,6 @@
 const NotifyClient = require("notifications-node-client").NotifyClient
-const template = require("./template")
 
-var client = new NotifyClient(process.env.NOTIFY_API_KEY)
+const client = new NotifyClient(process.env.NOTIFY_API_KEY)
 
 exports.handler = async (event, context, callback) => {
   try {
@@ -9,10 +8,13 @@ exports.handler = async (event, context, callback) => {
     let host = process.env.EMAIL_HOST || "https://scout-x.netlify.app"
 
     let res = await client.sendEmail(process.env.NOTIFY_TEMPLATE_ID, email, {
+      reference: null,
       personalisation: {
         pins: template(host, pins),
       },
     })
+
+    console.log(res)
 
     return {
       statusCode: 200,
@@ -30,3 +32,28 @@ exports.handler = async (event, context, callback) => {
     }
   }
 }
+
+const truncate = (str, noWords) => {
+  if (str && noWords > 1) {
+    if (str.split(" ").length > noWords) {
+      return str.split(" ").splice(0, noWords).join(" ") + "..."
+    } else {
+      return str
+    }
+  }
+}
+
+const template = (host, pins) =>
+  pins
+    .map(
+      pin => `
+      
+            # ${pin.name}
+
+            ${host}/service/${pin.id}
+
+            ${truncate(pin.description, 30)}
+
+        `
+    )
+    .join("")
