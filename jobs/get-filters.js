50438cf2c1bfd1c38bfb9adfbe20e30600cbd9de
Hannah <xhan104@gmail.com>
2021-08-23 14:18:25 +0100

    Update filters build to respect directory query

--- jobs/get-filters.js
+++ jobs/get-filters.js
@@ -1,10 +1,25 @@
 require("dotenv").config()
 const fetch = require("isomorphic-unfetch")
 const fs = require("fs")
+const validThemes = require("../src/themes/valid-themes.json")
+const valid = validThemes.valid
+const queryString = require("query-string")
+
+const themeLabel =
+  process.env.hasOwnProperty("REACT_APP_THEME") &&
+  valid.find(e => e.label === process.env.REACT_APP_THEME.toLowerCase())
+    ? process.env.REACT_APP_THEME.toLowerCase()
+    : "generic"
+
+const directory = [themeLabel]
 
 const fetchTaxonomies = async resource => {
   console.log(`Fetching ${resource} data...`)
-  const res = await fetch(`${process.env.FILTERS_DATASOURCE}/${resource}`)
+  const res = await fetch(
+    `${process.env.FILTERS_DATASOURCE}/${resource}?${queryString.stringify({
+      directory,
+    })}`
+  )
   const data = await res.json()
   fs.writeFile(`src/data/_${resource}.json`, JSON.stringify(data), err => {
     if (err) return console.log(err)
