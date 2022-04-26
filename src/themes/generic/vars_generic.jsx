a6a9d971b455dc25cc6a61bf40e49dd495e793a7
lizzie cullen davison <lizzie.cullendavison@gmail.com>
2022-04-26 12:32:50 +0100

    Remove references to Bucks council from generic theme and update FG as TPX

--- src/themes/generic/vars_generic.jsx
+++ src/themes/generic/vars_generic.jsx
@@ -1,14 +1,13 @@
 import React from "react"
 import logo from "./logo.svg"
-import * as Styles from "../../components/Footer/Footer.styles"
 
 export const vars_generic = {
   slug: "generic",
   title: "Scout",
-  contactEmail: "support@wearefuturegov.com",
+  contactEmail: "fg-support@tpximpact.com",
   serviceHomepageUrl: "https://scout-and-outpost.netlify.app/",
-  organisation: "FutureGov",
-  organisationUrl: "https://www.wearefuturegov.com",
+  organisation: "TPXimpact",
+  organisationUrl: "https://www.tpximpact.com",
   tagline: "Find activities and organisations near you",
   beta: true,
   headerLogo: logo,
@@ -17,8 +16,8 @@ export const vars_generic = {
   cookieMessage: (
     <>
       <p>
-        We use cookies to collect information about how you use the
-        Buckinghamshire Council website.
+        We use cookies to collect information about how you use the Scout
+        website.
       </p>
       <p>We use this information to improve the website and our services.</p>
     </>
@@ -27,25 +26,7 @@ export const vars_generic = {
   cookieCallback: () => {
     console.log("loadcookies")
   },
-  footerNav: (
-    <Styles.Nav>
-      <Styles.NavLink href="https://familyinfo.buckinghamshire.gov.uk/about/">
-        About the Buckinghamshire Online Directory
-      </Styles.NavLink>
-      <Styles.NavLink href="https://familyinfo.buckinghamshire.gov.uk/accessibility-statement-/">
-        Accessibility statement
-      </Styles.NavLink>
-      <Styles.NavLink href="https://familyinfo.buckinghamshire.gov.uk/privacy-policy/">
-        Privacy policy
-      </Styles.NavLink>
-      <Styles.NavLink href="https://www.buckinghamshire.gov.uk/about/cookies/">
-        Cookies
-      </Styles.NavLink>
-      <Styles.NavLink href="https://familyinfo.buckinghamshire.gov.uk/publiccustomer-disclaimer/">
-        Public and customer disclaimer
-      </Styles.NavLink>
-    </Styles.Nav>
-  ),
+  footerNav: null,
 }
 
 export default vars_generic
