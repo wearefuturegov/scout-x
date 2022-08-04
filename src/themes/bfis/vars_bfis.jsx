eb21b71bb11c4bf1bbabd94db7d78f415e79ab4b
Hannah <xhan104@gmail.com>
2022-08-04 16:24:45 +0100

    SUPPORT-20 bfis gta

--- src/themes/bfis/vars_bfis.jsx
+++ src/themes/bfis/vars_bfis.jsx
@@ -44,7 +44,20 @@ export const vars_bfis = {
   cookieName: "bfis-scout",
   cookieCallback: () => {
     console.log("cookieCallback")
-    // Google Tag Manager
+
+    // Google Tag Manager - new GA4
+    ;(function (w, d, s, l, i) {
+      w[l] = w[l] || []
+      w[l].push({ "gtm.start": new Date().getTime(), event: "gtm.js" })
+      var f = d.getElementsByTagName(s)[0],
+        j = d.createElement(s),
+        dl = l !== "dataLayer" ? "&l=" + l : ""
+      j.async = true
+      j.src = "https://www.googletagmanager.com/gtm.js?id=" + i + dl
+      f.parentNode.insertBefore(j, f)
+    })(window, document, "script", "dataLayer", "GTM-W9X4P7B")
+
+    // Google Tag Manager - original - remove after July 2023
     ;(function (w, d, s, l, i) {
       w[l] = w[l] || []
       w[l].push({ "gtm.start": new Date().getTime(), event: "gtm.js" })
