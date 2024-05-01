101fd4e025c9083adba0f84bd525e8d567f543de
Han <apricot13@users.noreply.github.com>
2024-05-01 14:27:53 +0100

    make taxonomy id an env var

--- src/themes/bfis-send/vars_bfis_send.jsx
+++ src/themes/bfis-send/vars_bfis_send.jsx
@@ -6,7 +6,6 @@ export const vars_bfis_send = {
   ...vars_bfis,
   slug: "bfis",
   title: "SEND Local Offer",
-  parentTaxonomyId: 518,
   headerComponents: (
     <SingleStrongLink
       text="How to use this directory"
