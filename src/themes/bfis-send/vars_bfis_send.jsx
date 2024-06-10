5f574fb12216ceb34bd13e38d85c196a4662cf36
Han <apricot13@users.noreply.github.com>
2024-06-10 18:04:22 +0100

    TOP-231 change title to BUCKS SEND Local Offer + update serviceHomepageUrl for bfis-send

--- src/themes/bfis-send/vars_bfis_send.jsx
+++ src/themes/bfis-send/vars_bfis_send.jsx
@@ -4,7 +4,8 @@ import { vars_bfis } from "./../bfis/vars_bfis"
 export const vars_bfis_send = {
   ...vars_bfis,
   slug: "bfis",
-  title: "SEND Local Offer",
+  title: "BUCKS SEND Local Offer",
+  serviceHomepageUrl: "https://familyinfo.buckinghamshire.gov.uk/send/",
   socialMedia: vars_bfis.socialMedia.map(sm => {
     if (sm.type === "facebook") {
       return {
